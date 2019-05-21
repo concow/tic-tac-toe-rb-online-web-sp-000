6699c80efea48eca6c2636dc03528bd67b22dc3b
Connor Cowan <ccowan003@comcast.net>
2019-05-21 20:35:34 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -19,14 +19,13 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-def move(board, index, value)
-  board[index] = value
-end
-
 def input_to_index(user_input)
   index = user_input.to_i - 1
 end
 
+def move(board, index, value)
+  board[index] = value
+end
 def valid_move?(board, index)
   index.between?(0,8) && !position_taken?(board, index)
 end
